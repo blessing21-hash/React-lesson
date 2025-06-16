302ccafd4bd65360bf2a728e19613753013b6e27
blessing21-hash <blessingkusiwani@gmail.com>
2025-06-16 11:19:46 +0200

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
 import DestinationCard from './components/DestinationCard';
 import './App.css';
 
