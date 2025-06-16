fa5b43b43d4e89b1e2ae9cf8cfffc23f7e21b136
blessing21-hash <blessingkusiwani@gmail.com>
2025-06-16 11:25:16 +0200

    latest update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <div>
       <p>&copy; 2025 MyTravelJournal. All rights reserved.</p>
-    </footer>
-  );
-};
+    </div>
+  )
+}
 
-export default Footer;
+export default Footer
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 const Header = () => {
   return (
-    <header className="header">
+    <div>
       <h1>My Travel Journal</h1>
-    </header>
-  );
-};
+    </div>
+  )
+}
 
-export default Header;
+export default Header
